e740ae9db824b2e79f12065fe6a6cf52b77aa90e
vijaya58 <64270430+vijaya58@users.noreply.github.com>
2020-06-16 09:31:12 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
     default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+        us-east-1 = "ami-085925f297f89fce1" # ubuntu 14.04 LTS
 		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
 		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
 		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
